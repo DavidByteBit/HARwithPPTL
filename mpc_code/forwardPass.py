b7761d2ebda25817ffcdafea7d31256c3e2903ba
David <David>
2021-11-02 16:01:15 -0700

    v0.0.6

--- mpc_code/forwardPass.py
+++ mpc_code/forwardPass.py
@@ -249,7 +249,7 @@ def dot_2d(x, y):
 
     # c = sfix.Array(len(x[0]))
 
-    @for_range(len(x))
+    @for_range_opt(len(x))
     def _(i):
         res[0] += sfix.dot_product(x[i], y[i])
     # res += x[i].dot(y[i])
