8ff6c85c5c73abc91520e3379821ac47fefdbb8d
David <David>
2021-11-09 14:22:28 -0800

    v0.0.6

--- mpc_code/forwardPass.py
+++ mpc_code/forwardPass.py
@@ -174,7 +174,7 @@ class Conv1D(Layer):
 
         # print("first time")
         # print(output)
-        @for_range_opt((self.filters, output_width))
+        @for_range_opt_multithread(threads, (self.filters, output_width))
         def _(i, j):
             # val = sfix.Matrix(self.kernel_h, self.kernel_w)
 
