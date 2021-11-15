e1c37e622296ed909d84c6edba35b845689778af
David <David>
2021-11-15 10:49:08 -0800

    v0.0.6

--- run_tests.py
+++ run_tests.py
@@ -12,6 +12,7 @@ target_id_loc = 0
 test_range_loc = 0
 train_cnn_loc = 0
 kshot_loc = 0
+comp_loc = 0
 
 original_file_contents = []
 with open(settings_path, 'r') as in_stream:
@@ -29,18 +30,21 @@ with open(settings_path, 'r') as in_stream:
             train_cnn_loc = counter
         if "kshot" in line:
             kshot_loc = counter
+        if "compile" in line:
+            comp_loc = counter
 
         counter += 1
 
 print("original file contents \n\n{a}\n".format(a="".join(original_file_contents)))
 
 
-def alter_settings(target_id_val, train_cnn_val, train_kshot_val):
+def alter_settings(target_id_val, train_cnn_val, train_kshot_val, comp_val):
     new_file_contents = original_file_contents[:]
 
     new_file_contents[target_id_loc] = str(" target_id: \"{a}\"\n".format(a=target_id_val))
     new_file_contents[train_cnn_loc] = str(" train_cnn: \"{a}\"\n".format(a=train_cnn_val))
     new_file_contents[kshot_loc] = str(" kshot: \"{a}\"\n".format(a=train_kshot_val))
+    new_file_contents[comp_loc] = str(" compile: \"{a}\"\n".format(a=comp_val))
 
     with open(settings_path, 'w') as out_stream:
         out_stream.write("".join(new_file_contents))
@@ -54,10 +58,13 @@ kshot_vals = [1, 5, 10]
 
 for k in kshot_vals:
     new_kshot_val = k
+    comp = "true"
     for i in range(num_of_participants - (target_start - 1)):
+        if i != (target_start - 1):
+            comp = "false"
         for _ in range(num_of_tests):
             new_target_id_val = i + target_start
             new_train_cnn = "true"
 
-            alter_settings(new_target_id_val, new_train_cnn, new_kshot_val)
+            alter_settings(new_target_id_val, new_train_cnn, new_kshot_val, comp)
             main.run_main(settings_path)
